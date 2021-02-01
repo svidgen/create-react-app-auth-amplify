9edfd4836cc3405fb52a28fe521e04b34a75451d
Jon Wire <wirej@amazon.com>
2021-02-01 17:39:48 -0600

    checkpoint

--- src/App.js
+++ src/App.js
@@ -1,32 +1,126 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { withAuthenticator } from 'aws-amplify-react'
-import Amplify, { Auth } from 'aws-amplify';
-import aws_exports from './aws-exports';
-Amplify.configure(aws_exports);
+import React, { Component, useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { ApolloClient, ApolloLink, gql, HttpLink, InMemoryCache } from "@apollo/client";
+import { withAuthenticator } from "aws-amplify-react";
+import Amplify, { Auth } from "aws-amplify";
+import { createAuthLink } from "aws-appsync-auth-link";
+import { createSubscriptionHandshakeLink } from 'aws-appsync-subscription-link';
+import aws_exports from "./aws-exports";
+// import { graphql } from "react-apollo";
+import { listNotes as listNotesQuery } from "./graphql/queries";
+import { createNote as createNoteQuery } from "./graphql/mutations"
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+let _client;
+
+let config = Amplify.configure(aws_exports);
+const listNotes = gql(listNotesQuery);
+const createNote = gql(createNoteQuery)
+
+async function get_client() {
+  if (_client) {
+    return _client;
   }
-}
+
+  let session = await Auth.currentSession();
+  
+  const aws_auth_config = {
+    url: aws_exports.aws_appsync_graphqlEndpoint,
+    region: aws_exports.aws_appsync_region,
+    auth: {
+      type: aws_exports.aws_appsync_authenticationType,
+      jwtToken: session.accessToken.jwtToken
+    }
+  };
+  const httpLink = new HttpLink({uri: aws_auth_config.url});
+  
+  const link = ApolloLink.from([
+    createAuthLink(aws_auth_config),
+    createSubscriptionHandshakeLink(aws_auth_config, httpLink)
+  ]);
+  
+  _client = new ApolloClient({
+    link,
+    cache: new InMemoryCache()
+  });
+
+  return _client;
+};
+
+function App() {
+
+  const [notes, setNotes] = useState([]);
+  const [newNoteName, setNewNoteName] = useState('');
+  const [newNoteDescription, setNewNoteDescription] = useState('');
+
+  // const { loading, error, notes } = useQuery(listNotes);
+
+  useEffect(() => {
+    // this is called after the component renders in the DOM.
+    // so ... anything we want to run after we're sure the thing
+    // is in the DOM goes here.
+    get_client().then(client => {
+      client.query({query: listNotes}).then(result => setNotes(result.data.listNotes.items));
+    });
+  });
+
+  // if (loading) return <p>Loading ...</p>;
+  // if (error) return <p>Error!</p>;
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log({
+      newNoteName, newNoteDescription
+    });
+    get_client().then(client => {
+      client.mutate({
+        mutation: createNote,
+        variables: {
+          input: {
+            name: newNoteName,
+            description: newNoteDescription
+          }
+        }
+      });
+    });
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div>
+          {notes.map((note) => (
+            <div>
+              <div>{note.name}</div>
+              <div>{note.description}</div>
+            </div>
+          ))}
+        </div>
+        <h4>Add a Note:</h4>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Name:</label>
+            <input
+              type='text'
+              value={newNoteName}
+              onChange={e => setNewNoteName(e.target.value)}
+            />
+          </div>
+          <div>
+            <label>Description:</label>
+            <textarea
+              value={newNoteDescription}
+              onChange={e => setNewNoteDescription(e.target.value)}
+            ></textarea>
+          </div>
+          <div>
+            <input type='submit' value='Add' />
+          </div>
+        </form>
+      </header>
+    </div>
+  );
+};
 
 export default withAuthenticator(App, true);
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,36 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateNote = /* GraphQL */ `
+  subscription OnCreateNote {
+    onCreateNote {
+      id
+      name
+      details
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateNote = /* GraphQL */ `
+  subscription OnUpdateNote {
+    onUpdateNote {
+      id
+      name
+      details
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteNote = /* GraphQL */ `
+  subscription OnDeleteNote {
+    onDeleteNote {
+      id
+      name
+      details
+      createdAt
+      updatedAt
+    }
+  }
+`;
